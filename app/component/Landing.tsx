f3e86d99f22345e03e5af2745c4bd41faccb7387
Nishant Guvvada <nishant.guvvada@gmail.com>
2024-08-17 11:37:10 +0530

    update

--- app/component/Landing.tsx
+++ app/component/Landing.tsx
@@ -17,7 +17,7 @@ export const LandingPage = () => {
                 <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NGX Wallet</h5>
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Start your crypto journey, create crypto wallets, just don't be a Scrooge!</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Start your crypto journey, create crypto wallets, just don&apos;t be a Scrooge!</p>
                 <a href="/getWallet" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Get started
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
