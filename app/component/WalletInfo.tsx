28075bd37d92e50b2b669e34b7448aa69a5c04a6
Nishant Guvvada <nishant.guvvada@gmail.com>
2024-08-18 22:45:04 +0530

    updated Wallet

--- app/component/WalletInfo.tsx
+++ app/component/WalletInfo.tsx
@@ -6,9 +6,12 @@ export const WalletInfo = () => {
     const [address, setAddress] = useState("");
     const [balance, setBalance] = useState();
     const [airdropSuccess, setAirdropSuccess] = useState(false);
+    const [airdropFailed, setAirdropFailed] = useState(false);
 
     async function fetchBalance() {
         try{
+            setAirdropFailed(false);
+            setAirdropSuccess(false);
             const response = await axios.post("https://api.devnet.solana.com", {
                 jsonrpc: "2.0",
                 id: 1,
@@ -34,6 +37,7 @@ export const WalletInfo = () => {
 
     async function airdrop() {
         try{
+            setAirdropFailed(false);
             const response = await axios.post("https://api.devnet.solana.com", {
                 jsonrpc: "2.0",
                 id: 1,
@@ -52,6 +56,7 @@ export const WalletInfo = () => {
             setAirdropSuccess(true);
         } catch(err) {
             console.log("error",err);
+            setAirdropFailed(true);
         }
     }
     return <div>
@@ -60,7 +65,16 @@ export const WalletInfo = () => {
             }} className="mb-2 max-w-md bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         <button onClick={fetchBalance} type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Get Balance</button>
         <button onClick={airdrop} type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Air Drop 1 SOL</button>
-        {airdropSuccess ? <div>success</div> : ""}
+        {airdropSuccess ? 
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span className="font-medium">Alert!</span> Success!
+            </div>
+        : ""}
+        {airdropFailed ? 
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span className="font-medium">Alert!</span> Failed!
+            </div> : ""
+        }
         {balance == null ? "" : <div className="max-w-sm p-4 mb-4 text-bold text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
             <span className="font-medium">Balance: </span> {balance / 1000000000} SOL
         </div>}
--- app/getWallet/page.tsx
+++ app/getWallet/page.tsx
@@ -4,11 +4,12 @@ import {generateMnemonic} from "bip39"
 import {GenerateWallet} from "../component/GetWallet";
 import { WalletInfo } from "../component/WalletInfo";
 
+
 export default function GetWallet() {
     const [mnemonic, setMnemonic] = useState("");
 
     return <div className="h-screen flex justify-center items-center">
-        
+        {/* {!wallet ? <WalletProvider wallets={wallets}><button onClick={onRequestConnectWallet} type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Connect Wallet</button></WalletProvider> : ""} */}
         <ol className="relative border-s border-gray-200 dark:border-gray-700">                  
             <li className="mb-10 ms-4">
                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -36,7 +37,6 @@ export default function GetWallet() {
                 
             </li>
         </ol>
-
-
     </div>
+
 }
